11b8d9ab860280eb311791d25fc9ffaef1573fdb
mxasan <xasannet@icloud.com>
2025-07-03 21:12:27 +0500

    config: add base in vite.configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/project-branch/', 
+  base: '/EdUmeed-mvp/', 
   plugins: [react()],
 })
